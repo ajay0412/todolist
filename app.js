7eef6ff8fd9dcfd5e253a8a5ff510fbd411f5890
MAHESH PEDDI <chityala044@gmail.com>
2023-09-29 18:12:06 +0530

    third commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const { error } = require("console");
 
-
+const port = 3000;
 
 const app = express();
 
@@ -117,6 +117,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || port, function() {
+  console.log("Server started on port");
 });
